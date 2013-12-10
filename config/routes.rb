d3685e380fcfd21af7d3a156e752ddea50daaa80
kuzmenko_max <kumaks09@yandex.ru>
2013-12-10 17:49:58 +0400

    543

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,7 @@ get "posts/search/"
   # match "/search" => "post#search"
   
  get "omniauth_callbacks/vkontakte"
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }   resources :users, :only => [:index, :destroy]
-
-
-
   devise_scope :users do
      get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru' 
  end
