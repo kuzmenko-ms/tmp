5c9e5bc6d5fb2e8403e0ccc4741eb44a2eb1b7fc
kuzmenko_max <kumaks09@yandex.ru>
2013-12-10 17:07:08 +0400

    543d

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ get "posts/search/"
   devise_for :users
   root :to => "posts#index"
 
-devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+ devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, :only => [:index, :destroy]
   root :to => 'users#index'
 
